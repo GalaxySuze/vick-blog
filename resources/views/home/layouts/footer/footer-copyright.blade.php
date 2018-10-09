d017c33482620bb4aaa27e46bb5f61084ed7a863
zhangweikang <weikang_zhang@loongjoy.com>
2018-10-09 17:22:37 +0800

    feat: 🎄 添加备案

--- resources/views/home/layouts/footer/footer-copyright.blade.php
+++ resources/views/home/layouts/footer/footer-copyright.blade.php
@@ -1,7 +1,7 @@
 <div class="footer-copyright">
     <div class="container">
         © 2017 - {{ date('Y', time()) }} Copyright Vick ` Blog. All Rights Reserved.
-        <a style="color: rgba(255,255,255,0.8)" href="http://www.miitbeian.gov.cn/">{{ env('DOMAIN_NAME_FILING') }}</a>
+        <a style="color: rgba(255,255,255,0.8)" href="http://www.miitbeian.gov.cn/">{{ env('DOMAIN_NAME_FILING', '浙ICP备17047367号-1') }}</a>
         <a class="grey-text text-lighten-4 right" href="#!">🌟🌟🌟🌟🌟</a>
     </div>
 </div>
