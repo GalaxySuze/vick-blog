c59049c3041efaf2ec61c11d4cce8b1c82cb761b
zhangweikang <weikang_zhang@loongjoy.com>
2018-10-09 17:09:17 +0800

    feat: 🎹 增加百度分析验证

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -2,6 +2,8 @@
 <html lang="{{ app()->getLocale() }}">
 <head>
     <meta name="google-site-verification" content="mAm5hsPteKA5julvrnSFvL0UE7il54v0S_Ft93sU8RI" />
+    <meta name="baidu-site-verification" content="QIImLJpR3N" />
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
